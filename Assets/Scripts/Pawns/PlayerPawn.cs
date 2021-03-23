5b3f7eb3be9d3faac2bb3c4a46a6f5bbb3bc417f
Sam Bossley <bossley.samuel@gmail.com>
2021-03-23 14:14:53 +0000

    Add thanos model and create template player prefab and pawn scripts

---
+++ Assets/Scripts/Pawns/PlayerPawn.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace MMC
+{
+    public class PlayerPawn : Pawn
+    {
+        void Start()
+        {
+            Debug.Log("Hello world!");
+        }
+
+        void Update()
+        { }
+    }
+}
