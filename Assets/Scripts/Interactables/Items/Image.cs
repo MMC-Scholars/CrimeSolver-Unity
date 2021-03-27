a9e334ab290b23f3bdeddb445e8c39f2e3dbdb64
William Delo <55678535+wdelo@users.noreply.github.com>
2021-03-26 20:18:47 -0400

    Made public properties since the inspector doesn't serialize properties.
    Made private fields serializable but with public property getters, e.g. in hand transform should be tweaked in editor but not changed anywhere else.

--- Assets/Scripts/Interactables/Items/Image.cs
+++ Assets/Scripts/Interactables/Items/Image.cs
@@ -7,9 +7,13 @@ namespace MMC
         //
         //  Declare reference to current billboard here
         //
-        public bool bIsPinned; // specifically "pinned" to a board
-        public bool bImagePinsPermanently; // theoretically an image can be pinned permanently (to some surface) but not be "pinned" to a board
-        public Vector3 vPinnedImageScale;
+
+        public bool IsPinned { get; set; } // specifically "pinned" to a board
+
+        [SerializeField]
+        bool imagePinsPermanently;
+        public bool ImagePinsPermanently { get { return imagePinsPermanently; } } // theoretically an image can be pinned permanently (to some surface) but not be "pinned" to a board
+
         public override void StartInteract(Pawn interacter, Vector3 hitLocation, Vector3 hitNormal)
         {
             PlayerPawn player = interacter as PlayerPawn;
--- Assets/Scripts/Interactables/Items/Item.cs
+++ Assets/Scripts/Interactables/Items/Item.cs
@@ -5,28 +5,24 @@ namespace MMC
 {
     public class Item : MonoBehaviour, IInteractable
     {
-        bool isInInventory;
-        public bool IsInInventory { get; protected set; }
-
-        bool isInHand;
-        public bool IsInHand { get; protected set; }
+        public bool IsInInventory { get; set; }
+        public bool IsInHand { get; set; }
 
         [SerializeField]
         Vector3 inHandPosition;
-        public Vector3 InHandPosition { get; }
+        public Vector3 InHandPosition { get { return inHandPosition; } }
 
         [SerializeField]
         Quaternion inHandRotation;
-        public Quaternion InHandRotation { get; }
-
+        public Quaternion InHandRotation { get { return inHandRotation; } }
 
         [SerializeField]
         Vector3 aimPosition;
-        public Vector3 AimPosition { get; }
+        public Vector3 AimPosition { get { return aimPosition; } }
 
         [SerializeField]
         Quaternion aimRotation;
-        public Quaternion AimRotation { get; }
+        public Quaternion AimRotation { get { return aimRotation; } }
 
         public virtual void StartInteract(Pawn interacter, Vector3 hitLocation, Vector3 hitNormal)
         {
