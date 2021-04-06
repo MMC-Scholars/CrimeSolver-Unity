397952c20494cffc0f4b9ff8ecd12722d8d88cc8
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-04-06 10:41:01 -0400

    Add right vector movement

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -6,7 +6,7 @@ namespace MMC
     public class PlayerController : MonoBehaviour
     {
         const float MOUSE_SENSITIVITY = 0.3f;
-        const float MOVEMENT_SPEED = 0.3f;
+        const float MOVEMENT_SPEED = 0.2f;
         // existing camera member is obsolete so we need the new keyword here
         // https://docs.unity3d.com/ScriptReference/Component-camera.html
         new Camera camera;
@@ -41,9 +41,8 @@ namespace MMC
 
         private void Move(Vector2 delta)
         {
-            Vector3 pos = transform.position;
-
-            transform.position = pos + playerMesh.transform.forward * delta.y * MOVEMENT_SPEED;
+            transform.position += playerMesh.transform.forward * delta.y * MOVEMENT_SPEED;
+            transform.position += playerMesh.transform.right * delta.x * MOVEMENT_SPEED;
         }
 
         public void OnMove(InputAction.CallbackContext value)
