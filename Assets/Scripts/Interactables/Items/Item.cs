212c8dc3a7046bb2bda96565ce4502c4b11dce7f
William Delo <55678535+wdelo@users.noreply.github.com>
2021-04-14 19:57:22 -0400

    Deleted extra field.

--- Assets/Scripts/Interactables/Items/Item.cs
+++ Assets/Scripts/Interactables/Items/Item.cs
@@ -11,15 +11,13 @@ namespace MMC
         public Vector3 InHandPosition { get; protected set; }
 
         [field: SerializeField]
-        Quaternion inHandRotation;
         public Quaternion InHandRotation { get; protected set; }
 
         [field: SerializeField]
         public Vector3 AimPosition { get; protected set; }
 
-        [SerializeField]
-        Quaternion aimRotation;
-        public Quaternion AimRotation { get { return aimRotation; } }
+        [field: SerializeField]
+        public Quaternion AimRotation { get; protected set; }
 
         public virtual void StartInteract(PlayerPawn player, Vector3 hitLocation, Vector3 hitNormal)
         {
